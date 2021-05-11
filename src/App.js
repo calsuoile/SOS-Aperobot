d4afcf58a48823b8f57e389dca613eb8048388f7
Charles-Hugo Vialle <charly.vialle@gmail.com>
2021-05-11 21:41:03 +0200

    progressBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Delivery from './view/Delivery';
 
 function App(props) {
   return (
     <div>
       <h1>SOS Apérobot</h1>
+      <Delivery />
       
     </div>
   );
